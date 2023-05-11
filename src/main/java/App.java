461afb3d69b27177341dc3dbbc798305ae3c9b9b
AntonShorin <Antnshorin2007@gmail.com>
2023-05-11 12:07:36 +0300

    Переделал гит

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        System.out.println("Переменные HelloWorld ссылаются на один и тот же объект? " + (bean == bean1));
+
+        Cat catbean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catbean.getMessage());
+        Cat catbean1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catbean1.getMessage());
+
+        System.out.println("Переменные Cat ссылаются на один и тот же объект? " + (catbean == catbean1));
+
     }
 }
